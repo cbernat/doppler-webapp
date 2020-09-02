e843330b74c9dbf184f21122d0f66467d3b06c90
cbernat <cbernat@makingsense.com>
2020-09-02 12:43:15 -0300

    chore: move hierarchy into plan-service file

--- src/services/plan-service.ts
+++ src/services/plan-service.ts
@@ -1,5 +1,4 @@
 import { DopplerLegacyClient } from './doppler-legacy-client';
-import { PlanHierarchy } from './plan-hierarchy';
 import {
   FreePath,
   StandardPath,
@@ -9,6 +8,31 @@ import {
   Plan,
   PathType,
 } from '../doppler-types';
+
+export interface PlanHierarchy {
+  getPaths(userPlan: PlanType): (FreePath | StandardPath | PlusPath | AgenciesPath)[];
+  // current plan free: FreePath, StandardPath, PlusPath, AgenciesPath
+  // current plan by credits: StandardPath, PlusPath, AgenciesPath
+  // current plan standard: StandardPath, PlusPath, AgenciesPath
+  // current plan plus: PlusPath, AgenciesPath
+
+  getPlanTypes(userPlan: Plan, pathType: PathType): PlanType[];
+  // get all plan types to be listed in slider: monthly-deliveries | subscribers | prepaid
+  // Free and select standard -->'PrepaidPack' | 'MonthlyRenewalDeliveriesPlan' | 'SubscribersLimitedPlan'
+
+  // Free and select plus --> 'MonthlyRenewalDeliveriesPlan' | 'SubscribersLimitedPlan'
+
+  // By credits -->  'PrepaidPack' | 'MonthlyRenewalDeliveriesPlan' | 'SubscribersLimitedPlan' (we should confirm this option)
+
+  // Standard and select standard (up contacts or up deliveries option) --> 'MonthlyRenewalDeliveriesPlan' | 'SubscribersLimitedPlan'
+
+  // Standard and select plus --> 'MonthlyRenewalDeliveriesPlan' | 'SubscribersLimitedPlan'
+
+  // plus and select plus (up contacts or up deliveries option) -->  'MonthlyRenewalDeliveriesPlan' | 'SubscribersLimitedPlan'
+
+  getPlans(userPlan: Plan, pathType: PathType, planType: PlanType): Plan[];
+  // this method is to actually get a plans array with all options to show in slider, in this method we must ensure to call BE once
+}
 
 export class PlanService implements PlanHierarchy {
   private PlanList: Plan[] = [];
