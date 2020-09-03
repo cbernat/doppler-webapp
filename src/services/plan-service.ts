056d2aa19a5fd0416a9c1f1908e4913647923ee7
cbernat <cbernat@makingsense.com>
2020-09-03 09:11:10 -0300

    chore: draft get paths

--- src/services/plan-service.ts
+++ src/services/plan-service.ts
@@ -10,7 +10,7 @@ import {
 } from '../doppler-types';
 
 export interface PlanHierarchy {
-  getPaths(userPlan: PlanType): (FreePath | StandardPath | PlusPath | AgenciesPath)[];
+  getPaths(userPlan: Plan): (FreePath | StandardPath | PlusPath | AgenciesPath)[];
   // current plan free: FreePath, StandardPath, PlusPath, AgenciesPath
   // current plan by credits: StandardPath, PlusPath, AgenciesPath
   // current plan standard: StandardPath, PlusPath, AgenciesPath
@@ -48,8 +48,75 @@ export class PlanService implements PlanHierarchy {
       : (this.PlanList = await this.dopplerLegacyClient.getAllPlans());
   }
 
-  getPaths(userPlan: PlanType): (FreePath | StandardPath | PlusPath | AgenciesPath)[] {
+  private getMinimumFee(path:PathType):number{
     throw new Error('Not implemented');
+  }
+
+  private isHigherPlan(plan:Plan):boolean {
+    throw new Error('Not implemented');
+  }
+
+  private getMinimumFeePrepaid(): number {
+    throw new Error('Not implemented');
+  }
+
+  getPaths(userPlan: Plan): (FreePath | StandardPath | PlusPath | AgenciesPath)[] {
+    const standardPath: StandardPath = {
+      type: 'standard',
+      actual: false,
+      minimumFee: this.getMinimumFee('standard'),
+      deadend: false,
+    };
+    const plusPath: PlusPath = {
+      type: 'plus',
+      actual: false,
+      minimumFee: this.getMinimumFee('plus'),
+      deadend: false,
+    };
+    const agenciesPath: AgenciesPath = {
+      type: 'agencies',
+      actual: false,
+      deadend: true,
+    };
+    const currentDeadend = this.isHigherPlan(userPlan); 
+    switch(userPlan.type){
+      case('free'):
+        const freePath: FreePath = {
+          type: 'free',
+          actual: true,
+          deadend: true
+        };
+       return [freePath, standardPath, plusPath, agenciesPath];
+      case('prepaid'):
+        const currentStandard: StandardPath = {
+          type: 'standard',
+          actual: true,
+          minimumFee: this.getMinimumFeePrepaid(), //minimum fee always
+          deadend: false, // if we have more plans this is true for prepaid always
+        };
+        return [currentStandard, agenciesPath];
+      case('subscribers'):
+        const currentPlanFee = userPlan.fee;
+        if (userPlan.featureSet === 'plus'){
+          const currentplusPath: PlusPath = {
+            type: 'plus',
+            actual: true,
+            minimumFee: currentPlanFee, // current plan fee
+            deadend: currentDeadend, // if there is no possibility to advance
+          };
+          return [currentplusPath, agenciesPath];
+        } else {
+          const currentStandard: StandardPath = {
+            type: 'standard',
+            actual: true,
+            minimumFee: currentPlanFee, //minimum fee always
+            deadend: currentDeadend, // if we have more plans this is true for prepaid always
+          };
+          return [currentStandard, plusPath, agenciesPath]
+        }
+      default: // if it's agencies should not get here
+        return []
+    }
   }
 
   getPlanTypes(userPlan: Plan, pathType: PathType): PlanType[] {
